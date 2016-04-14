b921ac45ab9df7b3b9dfd7385e4f9936a5605f16
Mauricio Vela <mauricioandresvela@gmail.com>
2016-04-13 20:54:35 -0400

    adaptIntegrate

--- PS7.R
+++ PS7.R
@@ -48,19 +48,91 @@ sg.int<-function(g,lower,upper, dimensions, parallel=1, ...){
 }
 
 
+###################################
+
+#TEST HAT
+library(testthat)
+
+#Check inputs
+test_that('Check inputs',{
+    g <- function(x) x[1]^2+x[2]^2
+    dimensions <- 2
+    lower <- c(0,0)
+    upper <- c(10,10)
+    expect_is(dimensions, "numeric")
+    expect_is(lower, "numeric")
+    expect_is(upper, "numeric")
+    expect_that(length(lower), equals(length(upper)))
+})
+
+#Check output 
+test_that("Check output", {
+  g <- function(x) x[1]^2+x[2]^2
+  dimensions <- 2
+  lower <- c(0,0)
+  upper <- c(10,10)  
+  int <- as.numeric(sg.int(g,lower=lower,upper=upper, dimensions=dimensions,parallel=2))
+  real_solution <- 20000/3
+  expect_equal(int, real_solution, tolerance=0.01)
+})
+
+
+#################################################
+#CHECK SPEED
+library(microbenchmark)
+#TWO DIMENSIONS
 g <- function(x) x[1]^2+x[2]^2
 lower <- c(0,0)
 upper <- c(10,10)
-sg.int(g,lower=lower,upper=upper, dimensions=2,parallel=2)
-
-
-
-
-library(microbenchmark)
 
 microbenchmark(
-  "1 core" = sg.int(g, dim=2, lower=lower, upper=upper, parallel=2),
+  "1 core" = sg.int(g, dim=2, lower=lower, upper=upper, parallel=1),
   "3 core" = sg.int(g, dim=2, lower=lower, upper=upper, parallel=3),
+  times=1
+)
+#Worst with 3 cores
+
+
+#THREE DIMENSIONS
+g <- function(x) x[1]^2+x[2]^2+x[3]^2
+lower <- c(0,0,0)
+upper <- c(10,10,10)
+
+microbenchmark(
+  "1 core" = sg.int(g, dim=3, lower=lower, upper=upper, parallel=1),
+  "3 core" = sg.int(g, dim=3, lower=lower, upper=upper, parallel=3),
+  times=1
+)
+#Still worst with 3 cores but no much difference as before 
+
+
+
+###################################
+#Include adaptIntegrate
+
+library(cubature)
+
+g <- function(x) x[1]^2+x[2]^2
+lower <- c(0,0)
+upper <- c(10,10)
+
+
+#Check if the results are equal
+
+test_that("Equal with adaptIntegrate", {
+  g <- function(x) x[1]^2+x[2]^2
+  dimensions <- 2
+  lower <- c(0,0)
+  upper <- c(10,10)  
+  int <- as.numeric(sg.int(g,lower=lower,upper=upper, dimensions=dimensions,parallel=2))
+  withadaptIntegrate <- adaptIntegrate(g, lower, upper)$integral
+  expect_equal(int, withadaptIntegrate, tolerance=0.01)
+})
+
+#Test speed
+microbenchmark(
+  "1 core" = sg.int(g, dim=3, lower=lower, upper=upper, parallel=1),
+  "3 core" = sg.int(g, dim=3, lower=lower, upper=upper, parallel=3),
   times=1
 )
 
